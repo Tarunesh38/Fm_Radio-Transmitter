e08b84d16d26f4f8ddd908eaff1572223212c0cd
Tarunesh38 <119646778+Tarunesh38@users.noreply.github.com>
2023-06-29 22:29:53 +0530

    The FM Radio Code Files

---
+++ fm_transmitter/transmitter.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "wave_reader.hpp"
+#include <mutex>
+
+class ClockOutput;
+
+class Transmitter
+{
+    public:
+        Transmitter();
+        virtual ~Transmitter();
+        Transmitter(const Transmitter &) = delete;
+        Transmitter(Transmitter &&) = delete;
+        Transmitter &operator=(const Transmitter &) = delete;
+        void Transmit(WaveReader &reader, float frequency, float bandwidth, unsigned dmaChannel, bool preserveCarrier);
+        void Stop();
+    private:
+        void TransmitViaCpu(WaveReader &reader, ClockOutput &output, unsigned sampleRate, unsigned bufferSize, unsigned clockDivisor, unsigned divisorRange);
+        void TransmitViaDma(WaveReader &reader, ClockOutput &output, unsigned sampleRate, unsigned bufferSize, unsigned clockDivisor, unsigned divisorRange, unsigned dmaChannel);
+        static void TransmitterThread(Transmitter *instance, ClockOutput *output, unsigned sampleRate, unsigned clockDivisor, unsigned divisorRange, unsigned *sampleOffset, std::vector<Sample> *samples, bool *stop);
+
+        ClockOutput *output;
+        std::mutex access;
+        bool stop;
+};
